ba8661f5294b0189bbbd6ca56048f4efea86bf7f
abdallaf92 <>
2021-04-18 12:09:19 +0000

    save

---
+++ App.js
@@ -0,0 +1,10 @@
+alert("welcome");
+var userInput = prompt("do you prefer Apple device or Android");
+
+if (userInput == "Apple"){
+  document.write("<img src = 'https://i2.wp.com/allhomecinema.com/wp-content/uploads/2020/12/iOS-7-10-Emblem.jpg?fit=1800%2C833&ssl=1'/>")
+
+}
+else if (userInput == "Android"){
+  document.write("<img src ='https://miro.medium.com/max/602/1*0n7jS9SZ2ODJIrQbEWsQkg.jpeg '/>")
+}
